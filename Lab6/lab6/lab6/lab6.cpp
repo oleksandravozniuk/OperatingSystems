6c87292811d2fd1cb833ba969f5599ea9cf661a5
alexandravoznyuk <16alexandra09@gmail.com>
2020-05-24 00:32:14 +0300

    lab 6

---
+++ Lab6/lab6/lab6/lab6.cpp
@@ -0,0 +1,36 @@
+﻿// lab6.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include<stdio.h>
+#include <string>
+#include <dos.h>
+#include <windows.h>
+
+using namespace std;
+
+void Draw(void)
+{
+	for (int i = 0; i < 100; i++)
+	{
+		//process of drawing
+		Sleep(1);
+	}
+
+	return;
+}
+
+
+int main(void)
+{
+	printf("\n Inside main()\n");
+	
+	int i = 0;
+
+	for (; i < 0xffffff; i++);
+	
+		Draw();
+		
+
+	return 0;
+}
